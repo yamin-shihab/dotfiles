5b571ed6ae8bb84ae128e8bed2812a77b0d2e486
yamin-shihab <yaminshihab123@gmail.com>
2022-01-02 15:21:33 -0700

    Removed Flameshot and some Fish stuff

--- fish/functions/tetrio.fish
+++
@@ -1,3 +0,0 @@
-function tetrio --wraps=/home/yamin/tetrio-desktop-8.0.0/tetrio-desktop --description 'alias tetrio /home/yamin/tetrio-desktop-8.0.0/tetrio-desktop'
-  /home/yamin/tetrio-desktop-8.0.0/tetrio-desktop $argv; 
-end
