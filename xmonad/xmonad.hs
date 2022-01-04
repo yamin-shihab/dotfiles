cf240b698a8e87b2e799c1e02422fcca0d01d00b
yamin-shihab <yaminshihab123@gmail.com>
2022-01-04 14:01:18 -0700

    Added yet ANOTHER Xmonad workspace: "term"

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -25,7 +25,7 @@ myBackgroundColor = "#282a36"
 myForegroundColor = "#f8f8f2"
 myNormalBorderColor = "#44475A"
 myFocusedBorderColor = "#6272A4"
-myWorkspaces = ["main", "web", "chat", "edit", "temp"]
+myWorkspaces = ["main", "term", "web", "chat", "edit", "temp"]
 
 -- Keybinds
 myAdditionalKeysP =
