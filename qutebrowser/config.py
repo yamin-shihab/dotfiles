92eba2f27ca85da92eb46f884b86379ee553aa6a
yamin-shihab <yaminshihab123@gmail.com>
2022-02-23 22:16:08 -0700

    Never gonna let you down

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -12,7 +12,7 @@ dracula.draw.blood(c, {
 
 c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
-c.downloads.location.directory = "/home/yamin/downloads/"
+c.downloads.location.directory = "$HOME/downloads/"
 c.fonts.default_family = "JetBrainsMono Nerd Font"
 c.fonts.default_size = "10pt"
 c.window.hide_decoration = True
