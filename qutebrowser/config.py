a2804229709a757450d11bccd7c33344761225bb
yamin-shihab <yaminshihab123@gmail.com>
2022-03-28 16:03:57 -0600

    Some guy is gonna call me "unprofessional" for these commit names.

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -17,3 +17,4 @@ c.fonts.default_family = "JetBrainsMono Nerd Font"
 c.fonts.default_size = "10pt"
 c.window.hide_decoration = True
 c.content.javascript.can_access_clipboard = True
+c.content.pdfjs = True
