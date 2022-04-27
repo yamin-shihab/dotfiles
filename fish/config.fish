54b119dbcdcac6593e1be76dee1531d1c9ece1ca
yamin-shihab <yaminshihab123@gmail.com>
2022-04-26 23:33:39 -0600

    XDG got drip

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,20 @@
-fish_add_path "$HOME/.local/bin" "$HOME/.cargo/bin"
+set -Ux XDG_CACHE_HOME "$HOME/.cache"
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -Ux XDG_DATA_HOME "$HOME/.local/share"
+set -Ux XDG_STATE_HOME "$HOME/.local/state"
+
+set -Ux CARGO_HOME "$XDG_DATA_HOME/cargo"
+set -Ux NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
+set -Ux RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+
+set -Ux BAT_THEME "Dracula"
+set -Ux CC "/usr/bin/clang"
+set -Ux CXX "/usr/bin/clang++"
+set -Ux EDITOR "/usr/bin/nvim"
+set -Ux PF_INFO "ascii title host kernel os shell uptime memory wm palette"
+
 zoxide init fish | source
-thefuck --alias | source
+fish_add_path "$HOME/.local/bin" "$XDG_DATA_HOME/cargo/bin"
 
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin "franciscolourenco/done"
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,9 +1,9 @@
-import dracula.draw
+import draw
 
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
+draw.blood(c, {
     'spacing': {
         'vertical': 6,
         'horizontal': 8
