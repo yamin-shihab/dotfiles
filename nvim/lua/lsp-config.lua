958ba26da013532d532fdf3d110f7a686ab9ac91
yamin-shihab <yaminshihab123@gmail.com>
2022-06-18 13:55:21 -0600

    Back to JetBrainsMono Nerd Font

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -77,12 +77,6 @@ return {
 		})
 		-- Nvim-cmp+nvim-autopairs
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		cmp.event:on(
-			"confirm_done",
-			cmp_autopairs.on_confirm_done({
-				map_char = { tex = "" },
-			})
-		)
-		cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Comic Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
