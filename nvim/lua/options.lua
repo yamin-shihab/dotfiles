98ff2126f800c3293164a999916c3a5c542a9e50
yamin-shihab <yaminshihab123@gmail.com>
2022-06-30 16:52:35 +0300

    Changed bash aliases/functions, skimmed neovim options, and added paru configs.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,5 @@
 -- Neovim options
 
-vim.opt.background = "dark"
 vim.opt.breakindent = true
 vim.opt.cursorline = true
 vim.opt.ignorecase = true
@@ -10,14 +9,11 @@ vim.opt.linebreak = true
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.ruler = true
 vim.opt.scrolloff = 5
-vim.opt.shiftwidth = 8
 vim.opt.showbreak = " > "
 vim.opt.showtabline = 0
 vim.opt.smartcase = true
 vim.opt.spell = true
-vim.opt.tabstop = 8
 vim.opt.termguicolors = true
 vim.opt.title = true
 vim.opt.ttimeoutlen = 0
