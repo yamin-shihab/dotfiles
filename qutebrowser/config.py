40f47cd4a93b7fb807a2e97f90d38b694bac3270
yamin-shihab <yaminshihab123@gmail.com>
2022-07-17 22:44:46 -0600

    Lots of changes and refactoring

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,10 +1,6 @@
 # Dracula
 import draw
 
-# Load existing settings made via :set
-config.load_autoconfig()
-
-# Dracula
 draw.blood(c, {
     'spacing': {
         'vertical': 6,
@@ -12,9 +8,16 @@ draw.blood(c, {
     }
 })
 
-c.hints.chars = "arstneio" # For Colemak
+
+# Keybinds
+config.bind("Z", "jseval var dracula=['7=282a36','8=f8f8f2','9=50fa7b','ae=t','t=p','s=m','w=n','m=l','o=s','j=282a36','a=p','aa=bd93f9','u=-1','x=f1fa8c','y=44475a','af=1','ai=1','f=1'];for(var i=0;i<dracula.length;i++)document.cookie=dracula[i];alert('Appearance settings have successfully been updated!');location.reload();")
+config.bind("z", "open -t https://raw.githubusercontent.com/qutebrowser/qutebrowser/master/doc/img/cheatsheet-big.png")
+
+
+# Options
+c.hints.chars = "arstneio"
 c.hints.mode = "letter"
-c.spellcheck.languages = [ "en-CA" ]
+c.spellcheck.languages = [ "en-US" ]
 c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.downloads.location.directory = "$HOME/downloads/"
@@ -23,9 +26,3 @@ c.fonts.default_size = "10pt"
 c.window.hide_decoration = True
 c.content.javascript.can_access_clipboard = True
 c.content.pdfjs = True
-
-# Cheatsheet
-config.bind("z", "open -t https://raw.githubusercontent.com/qutebrowser/qutebrowser/master/doc/img/cheatsheet-big.png")
-
-# Dracula for Duckduckgo
-config.bind("Z", "jseval var dracula=['7=282a36','8=f8f8f2','9=50fa7b','ae=t','t=p','s=m','w=n','m=l','o=s','j=282a36','a=p','aa=bd93f9','u=-1','x=f1fa8c','y=44475a','af=1','ai=1','f=1'];for(var i=0;i<dracula.length;i++)document.cookie=dracula[i];alert('Appearance settings have successfully been updated!');location.reload();")
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,3 @@
--------------
--- IMPORTS --
--------------
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
@@ -13,101 +10,98 @@ import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 
----------------
--- VARIABLES --
----------------
+
 -- Basic stuff
-mySpacing = 10
-
+mySpacing = 5
 myTerminal = "st"
-
 myModMask = mod4Mask
-
 myBorderWidth = 3
-
 myBackgroundColor = "#282a36"
-
 myForegroundColor = "#f8f8f2"
-
 myNormalBorderColor = "#44475A"
-
 myFocusedBorderColor = "#6272A4"
-
 myWorkspaces = ["main", "term", "web", "chat", "edit", "temp"]
 
+
+-- Keybinds
 myAdditionalKeysP =
-    [ ("M-S-'", spawn "slock systemctl suspend")
-    , ("M-'", spawn "slock")
-    , ("M-<Print>", spawn "flameshot gui")
-    , ("M-x", spawnAllStatusBars)
-    , ("M-z", sendMessage ToggleStruts)
-    , ("M-<XF86AudioRaiseVolume>", spawn "pulsemixer --change-volume 5")
-    , ("M-<XF86AudioLowerVolume>", spawn "pulsemixer --change-volume -5")
-    , ("M-<XF86AudioMute>", spawn "pulsemixer --toggle-mute")
-    , ("M-;", spawn "rofi -show drun")
-	, ("M-S-;", spawn "rofi -show window")
-    , ("M-c", spawn "setxkbmap -layout us -variant colemak")
-    , ("M-v", spawn "setxkbmap -layout us")
-    , ("M-`", spawn "feh $HOME/images/saved/colemak.png")
-    ] ++
-    -- Magic copy+paste weird Haskell code warning!
-    [ ( mask ++ "M-" ++ [key]
-      , screenWorkspace scr >>= flip whenJust (windows . action))
-    | (key, scr) <- zip "wfp" [0, 1, 2]
-    , (action, mask) <- [(W.view, ""), (W.shift, "S-")]
-    ]
-    -- Maybe choosing XMonad was a mistake...
+	[ ("M-S-'", spawn "slock systemctl suspend")
+	, ("M-'", spawn "slock")
+	, ("M-<Print>", spawn "flameshot gui")
+	, ("M-x", spawnAllStatusBars)
+	, ("M-z", sendMessage ToggleStruts)
+	, ("M-<XF86AudioRaiseVolume>", spawn "pulsemixer --change-volume 5")
+	, ("M-<XF86AudioLowerVolume>", spawn "pulsemixer --change-volume -5")
+	, ("M-<XF86AudioMute>", spawn "pulsemixer --toggle-mute")
+	, ("M-;", spawn "rofi -monitor -4 -show drun")
+	, ("M-c", spawn "setxkbmap -layout us -variant colemak")
+	, ("M-v", spawn "setxkbmap -layout us")
+	] ++
+	-- Magic copy+paste weird Haskell code warning!
+	[ ( mask ++ "M-" ++ [key]
+	, screenWorkspace scr >>= flip whenJust (windows . action))
+	| (key, scr) <- zip "wfp" [0, 1, 2]
+	, (action, mask) <- [(W.view, ""), (W.shift, "S-")]
+	]
+	-- Maybe choosing XMonad was a mistake...
+
 
 -- Looks
-myLayoutHook = avoidStruts $ tiled ||| noBorders Full
-  where
-    tiled = spacingWithEdge mySpacing $ Tall nmaster delta ratio
-    nmaster = 1
-    ratio = 1 / 2
-    delta = 3 / 100
+myLayoutHook = avoidStruts $ tiled ||| noBorders Full where
+	tiled = spacingWithEdge mySpacing $ Tall nmaster delta ratio
+	nmaster = 1
+	ratio = 1 / 2
+	delta = 3 / 100
+
 
 -- Startup garbaj
-myStartupHook = spawnAllStatusBars
+myStartupHook = do
+	spawnAllStatusBars
+	spawnOnce "picom --experimental-backends"
+	spawnOnce "dunst"
+	spawnOnce "xsetroot -cursor_name left_ptr"
+	spawnOnce "redshift -l 53.55014:-113.46871 -t 7000:5000"
+	spawnOnce "setxkbmap -layout us -variant colemak"
+	spawnOnce "xset r rate 300 40"
+	spawnOnce "xset r 66"
+	spawnOnce "xautolock -time 5 -locker slock"
+	spawnOnce "xbanish"
+	spawnOnce "livewallpaper"
 
--- Xmobar stuff
+
+-- XMobar stuff
 spawnAllStatusBars = do
-    killAllStatusBars
-    spawnStatusBar "xmobar -x 0"
-    spawnStatusBar "xmobar -x 1"
+	killAllStatusBars
+	spawnStatusBar "xmobar -x 0"
+	spawnStatusBar "xmobar -x 1"
 
 myXmobarColor fgColor = xmobarColor fgColor "#282a36"
 
-myXmobarPP =
-    def
-        { ppCurrent = myXmobarColor "#f1fa8c"
-        , ppVisible = myXmobarColor "#44475a"
-        , ppHidden = myXmobarColor "#44475a"
-        , ppUrgent = myXmobarColor "#ff5555"
-        , ppLayout = myXmobarColor "#bd93f9"
-        , ppTitle = myXmobarColor "#50fa7b"
-        , ppSep = " | "
-        }
+myXmobarPP = def
+	{ ppCurrent = myXmobarColor "#f1fa8c"
+	, ppVisible = myXmobarColor "#44475a"
+	, ppHidden = myXmobarColor "#44475a"
+	, ppUrgent = myXmobarColor "#ff5555"
+	, ppLayout = myXmobarColor "#bd93f9"
+	, ppTitle = myXmobarColor "#50fa7b"
+	, ppSep = " | "
+	}
 
 myXmobar = statusBarProp "xmobar" (pure myXmobarPP)
 
--- Just look at the name
-myMainConfig =
-    xmonad . ewmh . withSB myXmobar . docks $
-    def
-        { terminal = myTerminal
-        , modMask = myModMask
-        , borderWidth = myBorderWidth
-        , normalBorderColor = myNormalBorderColor
-        , focusedBorderColor = myFocusedBorderColor
-        , startupHook = myStartupHook
-        , layoutHook = myLayoutHook
-        , workspaces = myWorkspaces
-        } `additionalKeysP`
-    myAdditionalKeysP
 
----------------------
--- APPLYING CONFIG --
----------------------
+-- Just look at the name
+myMainConfig = xmonad . ewmh . withSB myXmobar . docks $ def
+	{ terminal = myTerminal
+	, modMask = myModMask
+	, borderWidth = myBorderWidth
+	, normalBorderColor = myNormalBorderColor
+	, focusedBorderColor = myFocusedBorderColor
+	, startupHook = myStartupHook
+	, layoutHook = myLayoutHook
+	, workspaces = myWorkspaces
+	} `additionalKeysP` myAdditionalKeysP
+
+
 -- Da config
 main = myMainConfig
--- And I hope that's pretty much it or something
