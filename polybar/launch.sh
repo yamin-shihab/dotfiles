d81fd577fb80d5055d234348562734f0ce7b8f61
Yamin Shihab <yaminshihab123@gmail.com>
2022-08-19 21:31:57 -0600

    More fixes to dwm, polybar, and bspwm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/sh
 killall polybar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	echo $m
-	MONITOR=$m polybar --reload mybar &
+	if [ $XDG_CURRENT_DESKTOP = "dwm" ]; then
+		wt="dwm"
+	else
+		wt="xworkspaces"
+	fi
+	echo $m $wt
+	MONITOR=$m WT=$wt polybar --config=$XDG_CONFIG_HOME/polybar/config.ini --reload mybar &
 done
 echo "Bars launched"
