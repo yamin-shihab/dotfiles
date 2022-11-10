144fb1311396d0d9b5955485db918032abb82d76
Yamin Shihab <yaminshihab123@gmail.com>
2022-11-09 17:12:56 -0700

    Switched from crouton to crostini, neovim and xorg updates

--- dwm/config.h
+++ dwm/config.h
@@ -3,7 +3,6 @@ static const char *fonts[]         = { "JetBrains Mono NL:size=10" };
 static const int showbar           = 1;
 static const int topbar            = 1;
 static const unsigned int borderpx = 2;
-static const unsigned int gappx    = 5 + borderpx * 2;
 static const unsigned int snap     = 32;
 static const char bg[]             = "#282a36";
 static const char fg[]             = "#f8f8f2";
@@ -13,7 +12,6 @@ static const char *colors[][3]     = {
 	//                fg  bg       border
 	[SchemeNorm]  = { fg, bg,      unfocused },
 	[SchemeSel]   = { fg, bg,      focused },
-	[SchemeTitle] = { bg, focused, focused },
 };
 
 // Tags
@@ -34,27 +32,22 @@ static const Layout layouts[] = {
 };
 
 // Keybinds
-// If on chromebook: #define MODKEY Mod1Mask
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,                       KEY,      view,           { .ui = 1 << TAG } }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     { .ui = 1 << TAG } }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            { .ui = 1 << TAG } }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      { .ui = 1 << TAG } },
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 #include "movestack.c"
 static const Key keys[] = {
 	//modifier          key            function          argument
 	{ MODKEY|ShiftMask, XK_Return,     spawn,            SHCMD("st") },
-	{ MODKEY,           XK_grave,      spawn,            SHCMD("croutoncycle cros") },
 	{ MODKEY,           XK_apostrophe, spawn,            SHCMD("i3lock-conf") },
 	{ MODKEY|ShiftMask, XK_apostrophe, spawn,            SHCMD("i3lock-conf & systemctl suspend") },
 	{ MODKEY,           XK_equal,      spawn,            SHCMD("pulsemixer --change-volume +5") },
 	{ MODKEY,           XK_minus,      spawn,            SHCMD("pulsemixer --change-volume -5") },
 	{ MODKEY,           XK_BackSpace,  spawn,            SHCMD("pulsemixer --toggle-mute") },
-	{ MODKEY|ShiftMask, XK_equal,      spawn,            SHCMD("volume up") },
-	{ MODKEY|ShiftMask, XK_minus,      spawn,            SHCMD("volume down") },
-	{ MODKEY|ShiftMask, XK_BackSpace,  spawn,            SHCMD("volume mute") },
 	{ MODKEY,           XK_i,          spawn,            SHCMD("iwctl station wlan0 connect Atheer") },
 	{ MODKEY|ShiftMask, XK_i,          spawn,            SHCMD("iwctl station wlan0 disconnect") },
 	{ MODKEY,           XK_s,          spawn,            SHCMD("flameshot gui") },
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,7 +2,7 @@
 vim.g.python_recommended_style = false
 vim.g.rust_recommended_style = false
 vim.opt.breakindent = true
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
 vim.opt.cursorline = true
 vim.opt.ignorecase = true
 vim.opt.laststatus = 3
