e96d1d5b41005919a5bd782c0fe85928eee34bba
Yamin Shihab <yaminshihab123@gmail.com>
2022-12-15 09:58:22 -0700

    oops...

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,17 +2,12 @@
 local lsp = require("lspconfig")
 local servers = { "rust_analyzer", "clangd", "bashls", "pyright", "sumneko_lua", "gdscript" }
 for _, server in ipairs(servers) do
-    lsp[server].setup(
-        require("chadtree").lsp_ensure_capabilities(
-            require("coq").lsp_ensure_capabilities({ on_attach = function() end })
-        )
-    )
+    lsp[server].setup({})
 end
-lsp.sumneko_lua.setup(require("chadtree").lsp_ensure_capabilities(require("coq").lsp_ensure_capabilities({
+lsp.sumneko_lua.setup({
     settings = {
         Lua = {
             diagnostics = { globals = { "vim" } },
         },
     },
-    on_attach = function() end,
-})))
+})
