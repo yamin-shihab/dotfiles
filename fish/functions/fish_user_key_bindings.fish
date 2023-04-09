efcdb4dd329cb75e3ef67cccbde6d4855b585729
Yamin Shihab <yaminshihab123@gmail.com>
2023-04-09 15:48:25 -0600

    Tweaked fish aliases and keybinds

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,7 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
+
+    bind -M insert \es "fish_commandline_prepend s"
+    bind -M visual \es "fish_commandline_prepend s"
+    bind \es "fish_commandline_prepend s"
 end
