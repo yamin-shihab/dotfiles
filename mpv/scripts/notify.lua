a54030c789acab3a0930ea52c89fdb759fbd4748
Yamin Shihab <yaminshihab123@gmail.com>
2023-06-19 19:42:17 -0600

    Notification progress bars!

--- mpv/scripts/notify.lua
+++ mpv/scripts/notify.lua
@@ -2,15 +2,14 @@ local function extract_art()
     local path = mp.get_property_native("path")
     os.execute(
         string.format(
-            'ffmpeg -loglevel quiet -y -i "%s" -an -vcodec copy /tmp/tmp_cover.png && ffmpeg -loglevel quiet -y -i /tmp/tmp_cover.png -vf "crop=min(in_w\\\\,in_h):out_w" /tmp/cover.png || cp -f /usr/share/icons/hicolor/128x128/apps/mpv.png /tmp/cover.png',
+            'ffmpeg -loglevel quiet -y -i "%s" -an -vcodec copy /tmp/tmp-cover.png && ffmpeg -loglevel quiet -y -i /tmp/tmp-cover.png -vf "crop=min(in_w\\\\,in_h):out_w" /tmp/cover.png || cp -f /usr/share/icons/hicolor/64x64/apps/mpv.png /tmp/cover.png',
             path
         )
     )
 end
 
 local function remove_art()
-    os.execute("rm -f /tmp/tmp_cover.png")
-    os.execute("rm -f /tmp/cover.png")
+    os.execute("rm -f /tmp/tmp-cover.png; rm -f /tmp/cover.png")
 end
 
 mp.observe_property("filename/no-ext", "native", function(_, filename)
@@ -29,6 +28,8 @@ mp.observe_property("pause", "native", function(_, paused)
         return
     end
 
+    local percent_pos = mp.get_property_native("percent-pos")
+
     if paused then
         paused = "Paused"
     else
@@ -36,6 +37,6 @@ mp.observe_property("pause", "native", function(_, paused)
     end
 
     extract_art()
-    os.execute(string.format('dunstify -r 2 -I /tmp/cover.png "%s:" "%s"', paused, filename))
+    os.execute(string.format('dunstify -r 2 -I /tmp/cover.png -h int:value:%d "%s:" "%s"', percent_pos, paused, filename))
     remove_art()
 end)
