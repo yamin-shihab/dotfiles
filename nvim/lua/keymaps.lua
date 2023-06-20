3e0815e88071a4d20200f4a2fad36842dcbdf035
Yamin Shihab <yaminshihab123@gmail.com>
2023-06-20 16:04:46 -0600

    Neovim keybinds and Fish aliases

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -12,12 +12,12 @@ vim.keymap.set({ "n", "x" }, "<Leader>w", ":lua require('nvim-window').pick()<CR
 -- LSP
 vim.keymap.set({ "n", "x" }, "<Leader>d", ":Lspsaga hover_doc<CR>")
 vim.keymap.set({ "n", "x" }, "<Leader>f", ":Lspsaga lsp_finder<CR>")
-vim.keymap.set({ "n", "x" }, "<Leader>j", ":Lspsaga diagnostic_jump_prev<CR>")
-vim.keymap.set({ "n", "x" }, "<Leader>k", ":Lspsaga diagnostic_jump_next<CR>")
+vim.keymap.set({ "n", "x" }, "<Leader>j", ":Lspsaga diagnostic_jump_next<CR>")
+vim.keymap.set({ "n", "x" }, "<Leader>k", ":Lspsaga diagnostic_jump_prev<CR>")
 vim.keymap.set({ "n", "x" }, "<Leader>r", ":Lspsaga rename<CR>")
 vim.keymap.set({ "n", "x" }, "<Leader>t", ":Lspsaga term_toggle<CR>")
 
 -- Hopping around
-vim.keymap.set({ "n", "x" }, "<Leader>h", ":HopWord<CR>")
 vim.keymap.set({ "n", "x" }, "<Leader><S-h>", ":HopLineStart<CR>")
+vim.keymap.set({ "n", "x" }, "<Leader>h", ":HopWord<CR>")
 vim.keymap.set({ "n", "x" }, "<Leader>s", ":HopPattern<CR>")
