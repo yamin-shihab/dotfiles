eaa95bddb8ad3903cb76fcb998698e465632c3ff
Yamin Shihab <yaminshihab123@gmail.com>
2025-07-11 23:58:12 -0600

    feat: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+abbr -a cp cp -iv
+abbr -a df df -h
+abbr -a du du -abhx -d 1
+abbr -a free free -h
+abbr -a grep grep -n
+abbr -a ip ip --color=auto
+abbr -a ls ls -hvAG --group-directories-first
+abbr -a mkdir mkdir -pv
+abbr -a mv mv -iv
+abbr -a rm rm -iv
+abbr -a sort sort -h
